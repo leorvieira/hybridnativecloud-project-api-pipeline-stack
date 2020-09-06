5f00065b061063a9cdbefa194ee0a332246a33d7
leorvieira <44712270+leorvieira@users.noreply.github.com>
2020-09-06 14:59:53 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -14,7 +14,7 @@ def main(event, context):
     
     
     responsePipeline = codepipeline.start_pipeline_execution(
-        name='hackathon-CD-app-stack-dev',
+        name='hackathon-CD-app-stack',
     )
     
     response = {
